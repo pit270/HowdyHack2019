118c51ac239acee0e18dda61863f3a779cc13d11
Chris Weeks <chrisrweeks@aol.com>
2019-09-08 09:32:31 -0500

    maybe fixed db path

--- HowdyHack2019/Properties/Settings.Designer.cs
+++ HowdyHack2019/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace HowdyHack2019.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\hackrDB.mdb")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=hackrDB.mdb")]
         public string hackrDBConnectionString {
             get {
                 return ((string)(this["hackrDBConnectionString"]));
